b2c6b11c8465167836e75b8522ef10175488e2a7
Taylor Monacelli <taylormonacelli@gmail.com>
2023-01-28 19:03:24 -0800

    Remove quick-steps prefix dir

--- vagrant/stuff3.sh
+++ vagrant/stuff3.sh
@@ -5,37 +5,37 @@ set -e
 set -u
 
 for host in master-1 master-2 loadbalancer worker-1 worker-2; do
-    rsync -a quick-steps $host: &
+    rsync -a quick-steps/ $host: &
     rsync -a cache/ $host: &
 done
 wait
 
 set +e
 
-# bash -x quick-steps/03a-master-1.sh
-bash -x quick-steps/03b-master-1.sh
-bash -x quick-steps/04a-master-1.sh </dev/null
-bash -x quick-steps/05a-master-1.sh </dev/null
-bash -x quick-steps/06a-master-1.sh
-bash -x quick-steps/07a-master-1-master2.sh
-ssh master-2 bash -x quick-steps/07a-master-1-master2.sh
-bash -x quick-steps/08a-master-1-master2.sh </dev/null
-ssh master-2 bash -x quick-steps/08a-master-1-master2.sh
-ssh loadbalancer bash -x quick-steps/08b-loadbalancer.sh
-ssh worker-1 bash -x quick-steps/09a-worker-1-worker-2.sh
-ssh worker-2 bash -x quick-steps/09a-worker-1-worker-2.sh
-bash -x quick-steps/10a-master-1.sh
+# bash -x 03a-master-1.sh
+bash -x 03b-master-1.sh
+bash -x 04a-master-1.sh </dev/null
+bash -x 05a-master-1.sh </dev/null
+bash -x 06a-master-1.sh
+bash -x 07a-master-1-master2.sh
+ssh master-2 bash -x 07a-master-1-master2.sh
+bash -x 08a-master-1-master2.sh </dev/null
+ssh master-2 bash -x 08a-master-1-master2.sh
+ssh loadbalancer bash -x 08b-loadbalancer.sh
+ssh worker-1 bash -x 09a-worker-1-worker-2.sh
+ssh worker-2 bash -x 09a-worker-1-worker-2.sh
+bash -x 10a-master-1.sh
 ssh -T worker-1 <<EOF
-    bash -x quick-steps/10b-worker-1.sh </dev/null
+    bash -x 10b-worker-1.sh </dev/null
 EOF
-bash -x quick-steps/10c-master-1.sh
-bash -x quick-steps/11a-master-1.sh
+bash -x 10c-master-1.sh
+bash -x 11a-master-1.sh
 ssh -T worker-2 <<EOF
-    bash -x quick-steps/11b-worker-2.sh </dev/null
+    bash -x 11b-worker-2.sh </dev/null
 EOF
-bash -x quick-steps/11c-master-1.sh
-bash -x quick-steps/12a-master-1.sh
-bash -x quick-steps/13a-master-1.sh
-bash -x quick-steps/14a-master-1.sh
-bash -x quick-steps/15a-master-1.sh
-bash -x quick-steps/16a-master-1.sh
+bash -x 11c-master-1.sh
+bash -x 12a-master-1.sh
+bash -x 13a-master-1.sh
+bash -x 14a-master-1.sh
+bash -x 15a-master-1.sh
+bash -x 16a-master-1.sh

5ea80308872939b7481ae1013a3d020efdd6e58d
Taylor Monacelli <taylormonacelli@gmail.com>
2023-01-28 18:48:16 -0800

    Remove my homemade scripts since we can extract documented ones from lab-script-generator.py

--- vagrant/s050.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -x
-set -e
-set -u
-
-# https://github.com/mmumshad/kubernetes-the-hard-way/blob/master/docs/06-data-encryption-keys.md#the-encryption-key
-scp encrypt.sh vagrant@192.168.56.11:/tmp/encrypt.sh
-ssh vagrant@192.168.56.11 bash -x /tmp/encrypt.sh
--- vagrant/stuff3.sh
+++ vagrant/stuff3.sh
@@ -1,35 +1,37 @@
 #!/usr/bin/env bash
 
 set -x
-# set -e
+set -e
 set -u
 
 for host in master-1 master-2 loadbalancer worker-1 worker-2; do
-    rsync -a quick-steps $host:
-    rsync -a cache/ $host:
+    rsync -a quick-steps $host: &
+    rsync -a cache/ $host: &
 done
+wait
+
+set +e
+
 # bash -x quick-steps/03a-master-1.sh
 bash -x quick-steps/03b-master-1.sh
-echo 1 1 | bash -x quick-steps/04a-master-1.sh
-echo 1 1 | bash -x quick-steps/05a-master-1.sh
+bash -x quick-steps/04a-master-1.sh </dev/null
+bash -x quick-steps/05a-master-1.sh </dev/null
 bash -x quick-steps/06a-master-1.sh
-# set +e
 bash -x quick-steps/07a-master-1-master2.sh
-# set -e
 ssh master-2 bash -x quick-steps/07a-master-1-master2.sh
-echo 1 1 | bash -x quick-steps/08a-master-1-master2.sh
+bash -x quick-steps/08a-master-1-master2.sh </dev/null
 ssh master-2 bash -x quick-steps/08a-master-1-master2.sh
 ssh loadbalancer bash -x quick-steps/08b-loadbalancer.sh
 ssh worker-1 bash -x quick-steps/09a-worker-1-worker-2.sh
 ssh worker-2 bash -x quick-steps/09a-worker-1-worker-2.sh
 bash -x quick-steps/10a-master-1.sh
 ssh -T worker-1 <<EOF
-    echo 1 1 | bash -x quick-steps/10b-worker-1.sh
+    bash -x quick-steps/10b-worker-1.sh </dev/null
 EOF
 bash -x quick-steps/10c-master-1.sh
 bash -x quick-steps/11a-master-1.sh
 ssh -T worker-2 <<EOF
-    echo 1 1 | bash -x quick-steps/11b-worker-2.sh
+    bash -x quick-steps/11b-worker-2.sh </dev/null
 EOF
 bash -x quick-steps/11c-master-1.sh
 bash -x quick-steps/12a-master-1.sh

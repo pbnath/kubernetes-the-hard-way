be0ab1276758e014deea7d9635a12102f5117f6f
Taylor Monacelli <taylormonacelli@gmail.com>
2023-01-29 10:54:50 -0800

    whitespace

--- vagrant/s190.sh
+++ vagrant/s190.sh
@@ -8,13 +8,11 @@ ssh -T vagrant@192.168.56.11<<'EOF'
 rm -rf quick-steps
 EOF
 
-
 rm -rf /Users/mtm/pdev/taylormonacelli/kubernetes-the-hard-way/quick-steps/
 python3 /Users/mtm/pdev/taylormonacelli/kubernetes-the-hard-way/tools/lab-script-generator.py --path /Users/mtm/pdev/taylormonacelli/kubernetes-the-hard-way/docs
 ls -1d /Users/mtm/pdev/taylormonacelli/kubernetes-the-hard-way/quick-steps/* | sort -n
 
 vagrant scp /Users/mtm/pdev/taylormonacelli/kubernetes-the-hard-way/quick-steps/ master-1:
-
 
 ssh -T vagrant@192.168.56.11<<'EOF'
     for host in master-1 master-2 loadbalancer worker-1 worker-2; do
